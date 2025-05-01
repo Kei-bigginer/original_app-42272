873bdf6c5e7ca747ea4ee6599e5675763d2f57a1
Kei-bigginer <new.world.dododo@gmail.com>
2025-05-01 21:25:32 +0900

    ストロングパラメーターを反映

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
+  private
+
+    # 🔐 Basic認証：外部から呼ばれない内部処理
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-  
 
-
-
-
+  # 🛠 Deviseで追加パラメータを許可（nickname, birthday）
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday])
+  end
 
 end
