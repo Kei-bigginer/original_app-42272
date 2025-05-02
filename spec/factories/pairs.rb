a90ecee6e12fdfb88c4b04eface5563ec32d56e7
Kei-bigginer <new.world.dododo@gmail.com>
2025-05-02 17:49:36 +0900

    コードコピー用のJS作成、単体テスト準備

--- spec/factories/pairs.rb
+++ spec/factories/pairs.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :pair do
-    anniversary { "2025-05-02" }
-    invite_code { "MyString" }
+    # ✅ anniversary は任意のため適当な日付をセット（なくても通る）
+    anniversary { "2025-05-01" }
+
+    # ✅ invite_code は自動生成されるので明示的には書かない（モデル内で生成されるため）
   end
 end
