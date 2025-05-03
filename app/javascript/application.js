2780b9a27dbb2fd83f310abe0c401fd30f75f92a
Kei-bigginer <new.world.dododo@gmail.com>
2025-05-03 22:06:50 +0900

    順番入替

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-// ✅ JavaScriptモジュールの読み込み（自作ファイルをインポート）
-import "./copy_invite_code"
+
 import "@rails/ujs"         // ✅ Importmap用に読み込む
 
 Rails.start()               // ✅ Rails UJSを有効化する
+
+// ✅ JavaScriptモジュールの読み込み（自作ファイルをインポート）
+import "./copy_invite_code"
