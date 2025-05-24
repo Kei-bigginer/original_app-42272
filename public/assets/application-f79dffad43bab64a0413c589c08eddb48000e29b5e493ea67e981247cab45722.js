9e7019b99ca11422fa21c5a48e1c8eb3085bedea
啓奨 <new.world.dododo@gmail.com>
2025-05-24 17:53:28 +0900

    Fix: Rails UJSとimportmap修正

---
+++ public/assets/application-f79dffad43bab64a0413c589c08eddb48000e29b5e493ea67e981247cab45722.js
@@ -0,0 +1,13 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// ✅ Rails UJS（リンクに method: :delete を効かせる）を読み込む
+import * as Rails from "@rails/ujs"
+Rails.start()
+
+// ✅ Turbo（ページ遷移を速くするライブラリ）
+import "@hotwired/turbo-rails"
+
+// ✅ Stimulus（JSでUI制御するライブラリ）
+import "controllers"
+
+// ✅ 自作ファイル（招待コードコピー処理）
+import "./copy_invite_code";
