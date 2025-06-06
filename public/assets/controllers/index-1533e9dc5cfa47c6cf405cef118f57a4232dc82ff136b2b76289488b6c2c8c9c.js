1fb5ec4da368ea1302316eaa4035cb587521c3dc
啓奨 <new.world.dododo@gmail.com>
2025-06-06 09:34:34 +0900

    🔥 remove any remaining rails-ujs links

--- public/assets/controllers/index-1533e9dc5cfa47c6cf405cef118f57a4232dc82ff136b2b76289488b6c2c8c9c.js
+++
@@ -1,8 +0,0 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
-
-import ExpandImageController from "./expand-image_controller" // 👈 明示的に読み込む
-
-application.register("expand-image", ExpandImageController)   // 👈 名前と紐づけ;
